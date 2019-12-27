b65b4ed10676b16eda27a8eedeef4268bdbc0268
Will Newell <newellwm@gmail.com>
2019-12-27 02:00:16 +0000

    Initialize with static file server
    Initialization involves the following:
        - Initialize node project with yarn
        - Add node_modules to .gitignore
        - Add nodemon for auto-restart on file change
        - Add node-static for simple static file server
        - Create root html file
        - Serve root directory from static file server

---
+++ index.js
@@ -0,0 +1,10 @@
+const static = require('node-static')
+const http = require('http')
+
+staticServer = new static.Server()
+
+http.createServer((req, res) => {
+    req.addListener('end', () =>
+        staticServer.serve(req, res)
+    ).resume()
+}).listen(3000)
