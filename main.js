b176e10250ee7dfcffb9cd5835816543049b5284
Will Newell <newellwm@gmail.com>
2019-12-28 18:46:31 +0000

    Add screen caputure code
    Currently the screen capture uses the same <video> as the webcam code
    (meaning once activated you can no longer see your cam).

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+"use strict"
 function hasUserMedia() {
-	return !!(navigator.mediaDevices.getUserMedia)
+	return !!(
+		navigator.mediaDevices.getUserMedia &&
+		navigator.mediaDevices.getDisplayMedia
+	)
 }
 
 function renderMedia(stream) {
@@ -11,11 +15,13 @@ function DisplayError(message) {
 	const main = document.querySelector('main')
 	const error = document.createElement('div')
 	error.className = 'error'
-	error.textContent = err
+	error.textContent = message
 	main.appendChild(error)
 }
 
 async function getMedia(constraints) {
+	const { getUserMedia } = navigator.mediaDevices
+	console.log(getUserMedia)
 	let stream = null;
 	try {
 		stream = await navigator.mediaDevices.getUserMedia(constraints)
@@ -25,6 +31,36 @@ async function getMedia(constraints) {
 	}
 }
 
+async function getCapture(constraints) {
+	let stream = null;
+
+	try {
+		stream = await navigator.mediaDevices.getDisplayMedia(constraints)
+		renderMedia(stream)
+	} catch(err) {
+		new DisplayError(err)
+	}
+}
+
 if (hasUserMedia) {
-	getMedia({ video: true, audio: true })
+	const camConfig = {
+		video: {
+			width: { ideal: 1280 },
+			height: { ideal: 720 }
+		},
+		audio: true
+	}
+	const screenConfig = {
+		video: {
+			cursor: 'always'
+		},
+		audio: {
+			echoCancellation: true,
+			noiseSuppression: true,
+			sampleRate: 44100
+		}
+	}
+	getMedia(camConfig)
+	const startBtn = document.querySelector('#capture')
+	startBtn.addEventListener('click', () => getCapture(screenConfig), false)
 }
