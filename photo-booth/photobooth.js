b940873f16abf4dcf790708ab4f96aa16f0189a8
Will Newell <newellwm@gmail.com>
2020-01-21 16:03:44 -0500

    Add filter effects to photo capture

--- photo-booth/photobooth.js
+++ photo-booth/photobooth.js
@@ -19,16 +19,21 @@ function initializeMedia(stream) {
 	const canvas = document.querySelector('canvas')
 	video.srcObject = stream
 	streaming = true
-	document.querySelector('#capture').addEventListener('click',
+	const filters = ['', 'grayscale', 'sepia', 'invert']
+	let currentFilter = 0
+	document.querySelector('video').addEventListener('click',
 		function(e) {
 			if (streaming) {
 				canvas.width = video.clientWidth
 				canvas.height = video.clientHeight
 				const ctx = canvas.getContext('2d')
 				ctx.drawImage(video, 0, 0)
+				currentFilter++
+				if(currentFilter > filters.length -1) currentFilter = 0
+				canvas.className = filters[currentFilter]
 			}
-	})
-
+		}
+	)
 }
 
 
